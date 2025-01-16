e3a8b88189a5f5ff1020fc9b21c0dd4d00c56317
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-01-16 20:00:49 +0100

    Added CLI

--- src/flatdraw/__main__.py
+++ src/flatdraw/__main__.py
@@ -1,19 +1,54 @@
+import argparse
+
 from .convert.image import ImageInterpreter
-from .convert import Color, Track
+from .convert import Track
+
+
+FLATDRAW_FONTART = """
+  ______ _       _      _                    
+ |  ____| |     | |    | |                   
+ | |__  | | __ _| |_ __| |_ __ __ ___      __
+ |  __| | |/ _` | __/ _` | '__/ _` \\ \\ /\\ / /
+ | |    | | (_| | || (_| | | | (_| |\\ V  V / 
+ |_|    |_|\\__,_|\\__\\__,_|_|  \\__,_| \\_/\\_/  
+"""
 
 
 def main():
-    print("FlatDraw")
+    parser = argparse.ArgumentParser(
+        prog="flatdraw",
+        description="Draw your flatland instances",
+        epilog="For more questions please consult our project repository on GitHub",
+    )
+    subparsers = parser.add_subparsers(dest="subparser")
+    convert_parser = subparsers.add_parser(
+        "convert", help="Convert an image to a flatland instance"
+    )
+    convert_parser.add_argument(
+        "image",
+        type=str,
+        help="Image to be converted to a flatland instance",
+    )
+    color_parser = subparsers.add_parser(
+        "color", help="Get the associated color of a track type"
+    )
+    color_parser.add_argument(
+        "track",
+        type=int,
+        help="Track type identifier of the flatland track to be converted",
+    )
+    args = parser.parse_args()
 
-    c = Color(255, 0, 255)
-    print(c.to_hex())
-
-    print(Track(32800).to_color().to_hex())
-    print(Track(1025).to_color().to_hex())
-    print(Track(38505).to_color().to_hex())
-
-    ii = ImageInterpreter("examples/crossing/test.png")
-    print(ii.convert())
+    if args.subparser == "convert":
+        ii = ImageInterpreter(args.image)
+        ii.convert()
+    elif args.subparser == "color":
+        t = Track(args.track)
+        print(t.to_color().to_hex())
+    else:
+        print(FLATDRAW_FONTART)
+        parser.print_help()
+    return
 
 
 if __name__ == "__main__":
