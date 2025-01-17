0b6ab45a630a8bb5623b005c97b0f0ce9aa5ae0d
Hannes Weichelt <hweichelt@uni-potsdam.de>
2025-01-17 17:44:44 +0100

    Updates

--- src/flatdraw/interactive/main.py
+++ src/flatdraw/interactive/main.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template
+import os
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+)
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/media/"
+ALLOWED_EXTENSIONS = {"png", "lp"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -10,4 +28,20 @@ def index():
 
 @app.route("/editor/")
 def editor():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(str(os.path.join(app.config["UPLOAD_FOLDER"], filename)))
+            return redirect(url_for("download_file", name=filename))
+
     return render_template("editor.html")
